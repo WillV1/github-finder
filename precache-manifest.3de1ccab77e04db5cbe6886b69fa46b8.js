9ada689118a9fc4cf15d566fd1efe37fb8175725
WillV1 <wmvanhook@gmail.com>
2020-07-29 06:10:47 -0400

    Updates

---
+++ precache-manifest.3de1ccab77e04db5cbe6886b69fa46b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1614bcd7fbf801d4815b4e7db9148b7",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "e8148862c477dc9b9368",
+    "url": "/github-finder/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0a4acf4e1d5bb5a9ce33",
+    "url": "/github-finder/static/css/main.c7160e2c.chunk.css"
+  },
+  {
+    "revision": "e8148862c477dc9b9368",
+    "url": "/github-finder/static/js/2.a994c4a7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/github-finder/static/js/2.a994c4a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a4acf4e1d5bb5a9ce33",
+    "url": "/github-finder/static/js/main.45487a98.chunk.js"
+  },
+  {
+    "revision": "189fbc883acbcb16644e",
+    "url": "/github-finder/static/js/runtime-main.c7dda347.js"
+  }
+]);
